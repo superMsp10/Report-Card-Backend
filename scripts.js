003c306d0fd5113fea641e54d632d82b87de46ce
superMsp10 <pandeymahan34@gmail.com>
2019-03-03 22:48:30 -0500

    Version 2 Improvements
    Update to ES6
    Applied changes according to following feedback:
    We noticed that your solution assumed that the ids in the files are incremented by one and are in order. We had test cases where this was not the case, and your solution could not handle those cases. Secondly, the method of calculating total average at the moment is incorrect.
    We like how you used the index of the array to make the solution more efficient, however, as you can see, it fails other test cases. Instead, consider storing the data in objects (i.e. hash tables), so you can efficiently reference information by id, but doesn't make any assumptions about those ids.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+import * as fs from 'fs';
 const dataPath = "data/";
 
 const studentsPath = dataPath + "students.csv";
@@ -7,14 +7,14 @@ const testsPath = dataPath + "tests.csv";
 const marksPath = dataPath + "marks.csv";
 const outputPath = "output.txt";
 
-var students = [];
-var courses = [];
-var tests = [];
-var marks = [];
-var studentToMarks = [];
+let students = [];
+let marks = [];
+let courses = {};
+let tests = {};
+let studentToMarks = {};
 
 
-function main(){
+function main() {
   parseByLine(studentsPath, parseStudents);
   parseByLine(coursesPath, parseCourses);
   parseByLine(testsPath, parseTests);
@@ -23,57 +23,57 @@ function main(){
   printReport();
 }
 
-function printReport(){
-  var report = "";
-  for (let index = 1; index < students.length; index++) {
-    var headLine;
-    var averageLine;
-    headLine = "Student Id: "+ index +", name: " + students[index] + "\n";
-    var _marks = studentToMarks[index];
+function printReport() {
+  let report = "";
 
-    var courseObjs = finalStudentMarksByCourse(_marks);
-    var markSum = 0;
-    var courseLines = "";
+  students.forEach(student => {
+    const index = student[0];
+    const name = student[1];
+    let headLine = `Student Id: ${index}, name: ${name} \n`;
+    let averageLine;
+    let _marks = studentToMarks[index];
+    let courseObjs = finalStudentMarksByCourse(_marks);
+    let markSum = 0;
+    let courseLines = "";
+    let courseCount = 0;
+
     courseObjs.forEach(courseSummative => {
-
+      courseCount++;
       markSum += courseSummative[2];
-      courseLines += "\tCourse: "+ courseSummative[0] + ", Teacher: " + courseSummative[1] + "\n"
-      courseLines += "\tFinal Grade: " + courseSummative[2].toFixed(2) +"%\n\n"
+      courseLines += `\tCourse: ${courseSummative[0]}, Teacher: ${courseSummative[1]} \n`
+      courseLines += `\tFinal Grade: ${courseSummative[2].toFixed(2)} %\n\n`
     });
-    
-   averageLine = "Total Average: "+ (markSum/(courseObjs.length-1)).toFixed(2) +"%\n\n"
-   report += headLine + averageLine + courseLines + "\n\n\n";
-  }
-  
-  fs.writeFile(outputPath, report, function(err) {
-    if(err) {
-        return console.log(err);
-    }
 
+    courseCount = courseCount == 0 ? 1 : courseCount;
+    averageLine = "Total Average: " + (markSum / courseCount).toFixed(2) + "%\n\n"
+    report += headLine + averageLine + courseLines + "\n\n\n";
+  });
+
+  fs.writeFile(outputPath, report, function (err) {
+    if (err) {
+      return console.log(err);
+    }
     console.log("The file was outputted!");
-}); 
+  });
 }
 
-function finalStudentMarksByCourse(_marks){
-  var courseObjs = [];
+function finalStudentMarksByCourse(_marks) {
+  let courseObjs = [];
 
   _marks.forEach(markIndex => {
-    var markObj = marks[markIndex];
+    let markObj = marks[markIndex];
+    let mark = markObj[1] / 100;
+    let testId = markObj[0];
+    let testObj = tests[testId];
+    let testWeight = testObj[1] / 100;
+    let courseId = testObj[0];
+    let courseObj = courses[courseId];
+    let weightedMark = mark * testWeight * 100;
 
-    var mark = markObj[1]/100;
-
-    var testId = markObj[0];
-    var testObj = tests[testId];
-    var testWeight = testObj[1]/100;
-
-    var courseId = testObj[0];
-    var courseObj = courses[courseId];
-    var weightedMark = mark * testWeight * 100;
-
-    if(courseObjs[courseId] == undefined){
+    if (courseObjs[courseId] == undefined) {
       //name, teacher, marksSum
       courseObjs[courseId] = [courseObj[0], courseObj[1], weightedMark];
-    }else{
+    } else {
       courseObjs[courseId][2] += weightedMark;
     }
   });
@@ -81,43 +81,42 @@ function finalStudentMarksByCourse(_marks){
   return courseObjs;
 }
 
-function parseByLine(path, callback){
-  var lines = fs.readFileSync(path, 'utf-8')
-  .split('\n')
-  .filter(Boolean);
+function parseByLine(path, callback) {
+  let lines = fs.readFileSync(path, 'utf-8')
+    .split('\n')
+    .filter(Boolean);
   //Skip first line, its a description of each coloumn
   for (let index = 1; index < lines.length; index++) {
     callback(lines[index]);
   }
 }
 
-function parseStudents(line){
-  var values = line.split(",");
+function parseStudents(line) {
+  let values = line.split(",");
   //id,name
-  students[values[0]] = values[1];
+  students.push([values[0], values[1]]);
 }
 
-function parseCourses(line){
-  var values = line.split(",");
+function parseCourses(line) {
+  let values = line.split(",");
   //id,name,teacher
   courses[values[0]] = [values[1], values[2]];
 }
 
-function parseTests(line){
-  var values = line.split(",");
+function parseTests(line) {
+  let values = line.split(",");
   //id,course_id,weight
   tests[values[0]] = [values[1], values[2]];
 }
 
-function parseMarks(line){
-  var values = line.split(",");
+function parseMarks(line) {
+  let values = line.split(",");
   //test_id,student_id,mark
-  var index = marks.push([values[0], values[2]]) - 1;
-  if( studentToMarks[values[1]] == undefined){
+  let index = marks.push([values[0], values[2]]) - 1;
+  if (studentToMarks[values[1]] == undefined) {
     studentToMarks[values[1]] = [];
   }
   studentToMarks[values[1]].push(index);
- 
 }
 
 
